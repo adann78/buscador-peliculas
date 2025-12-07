daa2b1df8bb1559e2b1bb58e5346706024d74b35
Adann78 <adanher78@gmail.com>
2025-12-07 11:59:06 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const apiKey ='02dd9b0b942942900fee5f905b0e1e39'
+document.getElementById('searchButton').addEventListener('click', searchMovies) 
+
+function searchMovies() {
+    let searchInput=document.getElementById('searchInput').value;
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchInput}`)
+    .then(response => response.json())
+    .then(data => {
+        let movies = data.results;
+        let moviesContainer = document.getElementById('results');
+        moviesContainer.innerHTML = '';
+        movies.forEach(movie => {
+            let movieDiv = document.createElement('div');
+            movieDiv.classList.add('movie');
+            movieDiv.innerHTML = `
+                <h3>${movie.title}</h3>
+                <p>Release Date: ${movie.release_date}</p>
+                <p>${movie.overview}</p>
+            `;
+            moviesContainer.appendChild(movieDiv);
+        });
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
